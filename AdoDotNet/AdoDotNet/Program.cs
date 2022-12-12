d6f1c8ea5524eaa46a7970cafe52baa943fdc0a0
faruqesay2 <faruqesay786@gmail.com>
2022-12-12 19:12:31 +0530

    Sql_Program

---
+++ AdoDotNet/AdoDotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdoDotNet
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcom To SQL Program");
+                EmployeeRepository.GetAllEmployee();
+                Console.ReadLine();
+            }
+        }
+    }
+}
+}
